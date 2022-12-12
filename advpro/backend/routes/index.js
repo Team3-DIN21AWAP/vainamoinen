ba8cca092a0161f6e557aec9efae7553d5d55c3b
SanttuSaleva <89972204+SanttuSaleva@users.noreply.github.com>
2022-12-12 20:26:15 +0200

    V5 done

--- advpro/backend/routes/index.js
+++ advpro/backend/routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { getUsers, Register, Login, Logout } from "../controllers/Users.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
+import {getV5} from "../controllers/V5.js";
  
 const router = express.Router();
  
@@ -10,5 +11,6 @@ router.post('/users', Register);
 router.post('/login', Login);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
+router.get('/v5', getV5);
  
 export default router;
